70b3a2c8c01eb9837b341b1bfcdf407f7ff99623
WGT <williamgeorgethomas@gmail.com>
2023-10-02 21:12:33 -0500

    added images and began styling About and Home page

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -3,11 +3,27 @@ import React from "react";
 const About = () => {
   return (
     <div className="flex flex-col items-center">
-      <h1 className="stylish text-4xl md:font-bold sm:font-semibold text-[#669BBC] drop-shadow-xl">What is Haem's Table?</h1>
-      <div className="text-center stylish text-xl w-1/2 md:font-semibold">big cakes, big taste, big booty</div>
-      {/* <h2 className="stylish text-3xl md:font-bold sm:font-semibold text-[#669BBC] drop-shadow-xl">How it works</h2> */}
-      <div className="stylish text-xl text-center md:font-semibold">
-        here is my life story blah blah blah<br></br>could be column idk<br></br>maybe just one block...depends
+      <h1 className="stylish text-4xl md:font-bold sm:font-semibold text-[#669BBC] drop-shadow-xl my-8">What is Haem's Table?</h1>
+      <div className="flex flex-row">
+        <div className="flex flex-col">
+          <div class="ml-12 mb-4 box-border h-72 w-96 p-4 border-4">
+            <img src="../images/nixconserv.jpg" alt="xxxxxxx"></img>
+          </div>
+          <div class="mb-4 box-border h-72 w-96 p-4 border-4">
+            <img src="../images/nixconserv.jpg" alt="xxxxxxx"></img>
+          </div>
+          <div class="mb-4 ml-12 box-border h-72 w-96 p-4 border-4">
+            <img src="../images/nixconserv.jpg" alt="xxxxxxx"></img>
+          </div>
+          <div class="box-border h-72 w-96 p-4 border-4">
+            <img src="../images/nixconserv.jpg" alt="xxxxxxx"></img>
+          </div>
+        </div>
+        <div className="text-center stylish mt-24 text-xl w-1/2 md:font-semibold">big cakes, big taste, big booty</div>
+        {/* <h2 className="stylish text-3xl md:font-bold sm:font-semibold text-[#669BBC] drop-shadow-xl">How it works</h2> */}
+        <div className="stylish text-xl mt-12 text-center md:font-semibold">
+          here is my life story blah blah blah<br></br>could be column idk<br></br>maybe just one block...depends
+        </div>
       </div>
     </div>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,19 +8,23 @@ import logo from "../logo.svg";
 // import About from "../components/About";
 
 const Home = () => {
-//   const { loading, data } = useQuery(QUERY_COURSES);
-//   const course = data?.courses || [];
+  //   const { loading, data } = useQuery(QUERY_COURSES);
+  //   const course = data?.courses || [];
 
   return (
     <main>
-      <div className="flex flex-col justify-center mb-4"></div>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-            a photo of a cake or something
-          </a>
-        </header>
+      <div className="flex flex-col justify-center m-4">
+        <div className="App flex flex-row">
+          <div class="coverPhoto">
+            <img src="../images/cabin.jpg" alt="xxxxxxx"></img>
+          </div>
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+              a photo of a cake or something
+            </a>
+          </header>
+        </div>
       </div>
     </main>
   );
