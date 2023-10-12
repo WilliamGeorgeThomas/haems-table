bca4c53bb86c29e53734f858e827cc904b9d3ad7
WGT <williamgeorgethomas@gmail.com>
2023-10-11 21:42:19 -0500

    added background image to Contact page. updated styling of contact form. fixed error on Contact page that moved Footer from the bottom of page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -4,17 +4,16 @@ import { Link } from "react-router-dom";
 const Contact = () => {
   return (
     <main className="flex flex-col items-center mt-12 p-8">
-      <div className="text-5xl font-bold grn mb-4">Contact Form</div>
-      <form className="flex flex-col items-center p-8 bg-offwhite rounded-lg">
-        <label className="font-bold">Email Address</label>
+      <div className="bg-photo"></div>
+      <form className="flex flex-col items-center border-4 border-[#476b40] rounded-lg p-8 bg-offwhite">
+        <div className="text-5xl font-bold salmon mb-4">Contact Form</div>
+        <label className="font-bold text-xl">Email Address</label>
+        <input type="text" className="border-4 border-[#fd874f] rounded-lg p-3 md:font-bold sm:font-semibold bg-offwhite" />
         <br />
-        <input type="text" className="rounded-border-4 border-[#fd874f] rounded-full p-3 md:font-bold sm:font-semibold" />
+          <label className="font-bold text-xl">Message</label>
+          <input type="text" className="border-4 border-[#fd874f] rounded-lg p-3 md:font-bold sm:font-semibold bg-offwhite" />
         <br />
-        <label className="font-bold">Message</label>
-        <br />
-        <input type="text" className="rounded-border-4 border-[#fd874f] rounded-full p-3 md:font-bold sm:font-semibold" />
-        <br />
-        <input type="submit" value="Submit!" className="border-[#fd874f] rounded-full font-bold text-2xl"></input>
+        <input type="submit" value="Submit!" className="border-4 border-[#fd874f] rounded-lg font-bold text-xl p-2 bg-offwhite"></input>
         {/* <Link to="/">Return Home</Link> */}
       </form>
     </main>
