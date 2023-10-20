328ec1c20606ef8d26de885695031c88d93c79a8
WGT <williamgeorgethomas@gmail.com>
2023-10-19 19:40:16 -0500

    restyled PageNotFound page

--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -3,9 +3,9 @@ import { Link } from "react-router-dom";
 
 const PageNotFound = () => {
   return (
-    <main className="flex flex-col items-center mt-12 p-8 stylish">
-      <div className="text-5xl font-bold text-[#669BBC]">Sorry, this page does not exist</div>
-      <button className="border-4 border-[#FDF0D5] rounded-full bg-[#d1d3aa] p-3 text-[#FDF0D5] stylish mr-8 md:font-bold sm:font-semibold drop-shadow-xl my-4">
+    <main className="flex flex-col items-center my-24 p-8">
+      <div className="text-5xl font-bold text-[#669BBC] my-8">Sorry, this page does not exist</div>
+      <button className="border-4 border-[#fd874f] rounded-lg font-bold text-xl p-2 bg-offwhite grn">
         <Link to="/">Return Home</Link>
       </button>
     </main>
