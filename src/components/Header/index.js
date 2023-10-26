74d919b275f98aeef242409add3363d1268a865d
WGT <williamgeorgethomas@gmail.com>
2023-10-25 19:58:56 -0500

    updated background color to a lighter shade of sandy color

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -11,6 +11,8 @@ const Header = () => {
           <h1 className="text-6xl offwhite molle logo drop-shadow-xl md:font-bold sm:font-semibold">Haem's Table</h1>
         </Link>
           <div className="flex flex-row flex-wrap gap-2">
+
+
             {/* <>
               <button className="border-4 border-[#fd874f] rounded-full bg-offwhite p-3 salmon mr-4 md:font-bold sm:font-semibold drop-shadow-xl">
                 <a href="#about">ABOUT</a>
