81f11710cafbacc4918f71627e71848021f91229
WGT <williamgeorgethomas@gmail.com>
2023-11-09 20:37:08 -0600

    began applying revised styling to Newsletter page

--- src/pages/Newsletter.js
+++ src/pages/Newsletter.js
@@ -3,18 +3,25 @@ import { Link } from "react-router-dom";
 
 const Newsletter = () => {
   return (
-    <main className="flex flex-col items-center mt-12 p-8">
-      <div className="bg-photo-po"></div>
-      {/* <div className="text-5xl font-bold grn mb-4">Subscribe to my newsletter!</div> */}
-        <form className="newsletter-form flex flex-col items-center border-4 border-[#476b40] rounded-lg p-8 bg-offwhite w-3/4 lg:w-1/2">
-          <label className="text-3xl font-bold grn text-center">Subscribe to my bi-weekly newsletter where I share stories, recipes, and upcoming news from Haem's Table!</label>
+    <main className="flex flex-col mt-12 p-8">
+      <div className="flex flex-row justify-evenly">
+        <div class="max-w-md">
+          <img src="../images/poboxes.jpg" alt="xxxxxxx"></img>
+        </div>
+        <div className="max-w-md">
+        <h1 className="text-2xl">Newsletter</h1>
+        <p className="text-xl">I write a bi-weekly newsletter where I share stories, recipes, and upcoming news from Haem's Table</p>
+        <p className="text-xl">Subscribe below!</p>
+        <form className="flex flex-row items-center w-3/4 lg:w-1/2 my-2">
+          {/* <label className="text-3xl font-bold grn text-center">Subscribe to my bi-weekly newsletter where I share stories, recipes, and upcoming news from Haem's Table!</label> */}
           <br />
-          <input type="text" placeholder="Your email address" className="border-4 border-[#fd874f] rounded-lg p-3 md:font-bold sm:font-semibold bg-offwhite" />
+          <input type="text" placeholder="Email address" className="border-2 border-[#fd874f] rounded-lg p-1 md:font-bold sm:font-semibold bg-offwhite" />
           <br />
-          <input type="submit" value="Submit!" className="border-4 border-[#fd874f] rounded-lg font-bold text-xl p-2 bg-offwhite"></input>
-          {/* <Link to="/">Return Home</Link> */}
+          <input type="submit" value="Submit!" className="ml-2 border-2 border-[#fd874f] rounded-lg font-bold p-1 bg-offwhite grn"></input>
         </form>
-      <div className="text-5xl font-bold salmon my-4">View archive?????</div>
+      <div className="text-xl salmon my-4">View archive</div>
+      </div>
+      </div>
     </main>
   );
 };
