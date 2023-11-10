c48b11f8ea74cb1eca0fc2776b413912ab690075
WGT <williamgeorgethomas@gmail.com>
2023-11-09 20:12:56 -0600

    further updated Contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -3,14 +3,14 @@ import { Link } from "react-router-dom";
 
 const Contact = () => {
   return (
-    <main className="flex flex-col items-center mt-12 p-8">
-      <div className="flex flex-row justify-between">
-        <div class="h-72 w-96 p-4">
+    <main className="flex flex-col mt-12 p-8">
+      <div className="flex flex-row justify-evenly">
+        <div class="max-w-md">
           <img src="../images/cabin.jpg" alt="xxxxxxx"></img>
-        </div>{" "}
-        <div>
+        </div>
+        <div className="max-w-md">
           <h1 className="text-2xl">Contact Me!</h1>
-          <p className="text-xl">Looking for a cake? Need savory treats for an event? Want to chat about your favorite foods?</p>
+          <p className="text-xl">Looking for a cake? Need savory treats for an event? Just want to chit chat about your favorite foods?</p>
           <p className="text-xl">Reach out via the form below!</p>
           <form className="flex flex-col items-center p-8">
             {/* <div className="text-5xl font-bold salmon mb-4">Contact Form</div> */}
@@ -20,8 +20,7 @@ const Contact = () => {
             {/* <label className="font-bold text-xl">Message</label> */}
             <input type="text" className="border-2 border-[#fd874f] rounded-lg px-1 py-12 md:font-bold sm:font-semibold bg-offwhite" placeholder="Message" />
             <br />
-            <input type="submit" value="Submit!" className="border-4 border-[#fd874f] rounded-lg font-bold text-xl p-2 bg-offwhite grn"></input>
-            {/* <Link to="/">Return Home</Link> */}
+            <input type="submit" value="Submit!" className="border-2 border-[#fd874f] rounded-lg font-bold text-xl p-2 bg-offwhite grn"></input>
           </form>
         </div>
       </div>
